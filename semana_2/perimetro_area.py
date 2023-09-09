117a0eaed7524922feca64c8910b00d7aab39bbb
cassiano <cassianothiago@rocketmail.com>
2023-09-09 09:23:24 -0400

    Update perimetro_area.py

--- semana_2/perimetro_area.py
+++ semana_2/perimetro_area.py
@@ -1,4 +1,5 @@
 lado=int(input('Digite o lado do quadradro: '))
 x=4*lado
 y=lado*lado
-print('O perímetro do quadrado de lado {} = {}\nA área do quadrado de lado {} = {}'.format(lado,x,lado,y))
+print(x)
+print(y)

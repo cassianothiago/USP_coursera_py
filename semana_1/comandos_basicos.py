d26bb1bf735d46691d6b3e836159d94c6808bead
cassiano <cassianothiago@rocketmail.com>
2023-09-08 22:05:25 -0400

    Update comandos_basicos.py

--- semana_1/comandos_basicos.py
+++ semana_1/comandos_basicos.py
@@ -10,3 +10,45 @@ print('a soma de a+b = ',soma)
 
 frase='a frase é uma frase'
 print(frase)
+
+c=type(frase)
+print(c)
+
+d=2.3456887852
+print(int(d))
+
+f=len(frase)
+print(f)
+
+g=str(d)
+print(g)
+print(type(g))
+print(len(g))
+
+h='bom'
+i='dia'
+j=len(h)+len(i)
+print(j)
+u=6/3 #float
+o=6//3 #inteiro
+print(u)
+print(o)
+
+a = 10
+b = 20
+c = a
+b = c
+a = b
+print(a,b,c)
+
+
+A = 10
+a = 20
+a = 2 * a
+A = a + A
+print(a)
+
+print ("olá" 'mundo')
+
+
+
